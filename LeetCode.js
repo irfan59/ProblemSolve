6efe61dc5452bc20c248cc688696f7d1dedf7ab8
Irfan saiyed <132121226+irfan59@users.noreply.github.com>
2026-01-09 05:42:03 +0000

    Irfan: 09/01/2026

--- LeetCode.js
+++ LeetCode.js
@@ -1,1 +1,16 @@
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * @date 09/01/2026
+ */
+var twoSum = function (nums, target) {
+    let temp = nums[0]
+    for (i = 0; i <= nums.length; i++) {
+        for (j = i+1; j <= nums.length; j++)
+            if (nums[i] + nums[j] == target) return [i, j]
+    }
+};
+
+console.log(twoSum([3,2,4], 6));
